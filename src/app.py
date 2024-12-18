b278f8763a11b1afcb68c024d876887cf7396db7
Mercedes Salaverri <mercedes.salaverri@gmail.com>
2024-12-18 21:15:39 +0000

    First Commit

--- src/app.py
+++ src/app.py
@@ -2,6 +2,14 @@ import os
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
+import spotipy
+
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # load the .env file variables
 load_dotenv()
+
+#Insert the information to work with it
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+
